25bcc94b264f3e87235ba9420771c8c4d087da53
Sateesh Turlapati <sateesh.turlapati@gmail.com>
2019-12-20 00:15:58 -0800

    Modified Server.js

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
-var express = require('express')
-var serveStatic = require('serve-static')
+var express = require('express');
+var app = express();
 
-var app = express()
+app.set('port', (process.env.PORT || 5000));
 
-app.use(serveStatic('dist', { 'index': ['default.html', 'default.htm'] }))
-app.listen(8000)
+app.use(express.static(__dirname + '/'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/partials');
+app.set('view engine', 'htm');
+
+app.get('/', function(request, response) {
+  response.render('default.htm');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
